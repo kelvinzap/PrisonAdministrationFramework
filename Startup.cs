5032f8f61d627e08323054ce282b9d9c1ec48aaf
kelvinzap <chisom859@gmail.com>
2022-01-19 02:28:06 +0100

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PrisonAdministrationSystem.Startup))]
+namespace PrisonAdministrationSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
